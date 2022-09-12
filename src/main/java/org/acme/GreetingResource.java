65834673fcd732ae3f542604bea40cbb365257a2
Georg Modzelewski <georg@redhat.com>
2022-09-12 14:55:39 +0200

    Add reflection registration to enable reflection manually where access only occurs from external and the aot compiler does not recognize it automatically

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,11 +1,14 @@
 package org.acme;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
+@RegisterForReflection
 public class GreetingResource {
 
     @GET
